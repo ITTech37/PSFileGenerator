3a1dbb46e4d4a58e4823356e99e21ddb447abe24
ITTech37 <45128030+ITTech37@users.noreply.github.com>
2020-01-06 21:47:24 +0100

    Add files via upload

---
+++ File_generator.ps1
@@ -0,0 +1,13 @@
+﻿######### Variables ##########
+
+$Filename = [System.IO.Path]::GetRandomFileName() + ".txt" # Generate a random filename
+$DocumentPath = [Environment]::GetFolderPath("MyDocuments") + "\"
+$FileLocation = $DocumentPath + $Filename
+
+########## Main #############
+
+if (!$args[0]) {
+    Write-Host "Please provide a value, eg : 2000000 for 2MB"
+} else {
+    fsutil file createnew $FileLocation $args[0]
+}
