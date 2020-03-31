7ec89f2aa6d4ac7bf73576dd8bfba7284759c9c5
PSGuy <45128030+ITTech37@users.noreply.github.com>
2020-03-31 09:52:49 +0200

    Some fixes
    - add some comments
    - convert user number in MB

--- File_generator.ps1
+++ File_generator.ps1
@@ -1,13 +1,14 @@
 ﻿######### Variables ##########
 
 $Filename = [System.IO.Path]::GetRandomFileName() + ".txt" # Generate a random filename
-$DocumentPath = [Environment]::GetFolderPath("MyDocuments") + "\"
+$DocumentPath = [Environment]::GetFolderPath("MyDocuments") + "\" # Find your default Documents folder location
 $FileLocation = $DocumentPath + $Filename
+$FileSize = "$args" + "000000" # Convert user input number in MegaBytes
 
-########## Main #############
+########### Main #############
 
 if (!$args[0]) {
-    Write-Host "Please provide a value, eg : 2000000 for 2MB"
+    Write-Host "Please provide a value, eg : 2 for 2MB"
 } else {
-    fsutil file createnew $FileLocation $args[0]
+    fsutil file createnew $FileLocation $FileSize # Generate file
 }
